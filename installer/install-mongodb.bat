1575abba737c30f3937f4ff6a262333207a583b0
epam297-lab <epam297@gmail.com>
2026-06-30 03:18:22 +0300

    Add installer/install-mongodb.bat

---
+++ installer/install-mongodb.bat
@@ -0,0 +1,22 @@
+@echo off
+title Install MongoDB
+cls
+echo ============================================
+echo     MongoDB Installation Helper
+echo ============================================
+echo.
+echo This will open the MongoDB Community Server download page.
+echo.
+echo Steps:
+echo 1. Download MongoDB Community Server (Windows x64 MSI)
+echo 2. Run the installer - keep all default settings
+echo 3. Make sure "Install MongoDB as a Service" is CHECKED
+echo 4. Complete installation
+echo 5. Run setup.bat to start the Visa Portal
+echo.
+echo Opening download page...
+start https://www.mongodb.com/try/download/community
+echo.
+echo After installing MongoDB, run setup.bat
+echo.
+pause
