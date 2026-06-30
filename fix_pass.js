ae8b0e7335162deae58c52d7c16596bba574aa68
epam297-lab <epam297@gmail.com>
2026-06-30 03:18:09 +0300

    Add fix_pass.js

---
+++ fix_pass.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/visa_portal').then(async () => {
+  const schema = new mongoose.Schema({}, { strict: false, collection: 'clients' });
+  const Client = mongoose.model('Client', schema);
+  await Client.updateOne({ passport: 'KN11223344' }, { $set: { password: 'norway2026' } });
+  await Client.updateOne({ passport: 'KO345678' }, { $set: { password: 'serbia2026' } });
+  await Client.updateOne({ passport: 'AU998877' }, { $set: { password: 'australia2026' } });
+  await Client.updateOne({ passport: 'SK556644' }, { $set: { password: 'slovakia2026' } });
+  console.log('All passwords updated to 2026 versions!');
+  process.exit(0);
+});
